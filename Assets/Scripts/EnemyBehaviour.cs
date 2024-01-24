6be55504949e3167aa02e6698e3ed6acc15b90b1
Aqueous <45310273+Crazygateway@users.noreply.github.com>
2024-01-24 21:56:35 +0000

    enemyai

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    // Variables
+    [SerializeField] GameObject targetObject;
+
+    [SerializeField] float objectSpeed;
+
+    Vector2 targetPosition;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPosition = targetObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetPosition = targetObject.transform.position;
+
+        transform.position = Vector2.MoveTowards(transform.position, targetPosition, objectSpeed * Time.deltaTime);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "player") Debug.Log("attacked player");
+    }
+}
