d696004be0531c5e0a26ab1cd5cd16be8b583bd2
Aqueous <45310273+Crazygateway@users.noreply.github.com>
2024-01-26 14:00:28 +0000

    menu additions
    - added menu backgrounds (+ filters)
    - added logo to main menu

---
+++ Assets/Scripts/ConditionMenuBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ConditionMenuBehaviour : MonoBehaviour
+{
+    public void OnRestartButtonClicked()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnQuitButtonClicked()
+    {
+        Application.Quit();
+        Debug.Log("Application quit");
+    }
+}
